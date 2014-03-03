826a3466d4d5d1614e99ed4d21955a4af5b3def6
Konstantin Konev <ksen.knev@gmail.com>
2014-03-03 20:54:56 +0200

    init

---
+++ RoslynPoweredPluginRegistration/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace RoslynPoweredPluginRegistration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Roslyn.Compilers;
+
+    using Roslyn.Compilers.CSharp;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var syntaxTree = SyntaxTree.ParseFile(@"..\..\Registration.cs");
+
+            var arrayCreation =
+                (ArrayCreationExpressionSyntax)syntaxTree.GetRoot().DescendantNodes().First(n => n.Kind == SyntaxKind.ArrayCreationExpression);
+
+            var arrayInitialization = (InitializerExpressionSyntax)arrayCreation.DescendantNodes().First(x => x.Kind == SyntaxKind.ArrayInitializerExpression);
+
+
+            var expressions = arrayInitialization.Expressions;
+
+            foreach (var e in expressions)
+            {
+                var memberAccess = (MemberAccessExpressionSyntax)e;
+
+                var name = memberAccess.Name.ToString();
+            }
+
+            var statement = arrayCreation.GetText();
+
+            Console.Write(statement);
+        }
+    }
+}
